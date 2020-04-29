6e8b6d9c9e7f560a026d0ce2da9a96d30359b7de
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-29 16:55:35 -0400

    Add more comments

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
+// Require all the packages
 const express = require("express");
 const mongoose = require("mongoose");
 
 const db = require("../models");
 const app = express();
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
